d53c9679e6387590f85b58413612c47e2f37d427
Ethan Sachs <72213447+ecars42@users.noreply.github.com>
2022-11-14 18:12:23 -0700

    Add files via upload

---
+++ src/pages/ethanSachs.js
@@ -0,0 +1,123 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hello, Im Ethan Sachs!</h1>
+          </div>
+          <img
+            className='pic'
+            src='https://pbs.twimg.com/profile_images/415961110635110400/xPO9YqTC.jpeg'
+            alt='Ethan Sachs'
+          />
+
+          <p className='description'>
+            This is all about me. I'm from New Jersey and love bagels like a true newjersyian. I like to snowboard and like computer science
+            specifically, I like Artificial Intelligence and Machine Learning but am willing to epxlore the field. I recently was initiated 
+            into AEPi, a fraternity here at CU Boulder. I'm looknig forward to learning more about github
+            and appreciate the project that you have assigned, although I am struggling to pick git up. I like to listen
+            to music, specifically I like melodic rap from artists like Mac Miller and Travis Scott. I also listen to
+            a wide variety of genres <b>BESIDES</b> country. I do not like country.
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
